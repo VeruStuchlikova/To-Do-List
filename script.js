b7ba410036098a02ceb85ff22915d827f1fa9879
Veronika Stuchlíková <stuchlikova.veronika@post.cz>
2021-05-03 19:37:06 +0200

    todoDiv in JS

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// selectors
+const todoInput = document.querySelector(".todo_input");
+const todoButton = document.querySelector(".todo_btn");
+const todoList = document.querySelector(".todo_list")
 
+// event listeners
+todoButton.addEventListener('click', addTodo);
+
+// functions
+function addTodo(event) {
+    event.preventDefault();
+    
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "Hey";
+    newTodo.classList.add('todo_item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed_btn');
+    todoDiv.appendChild(completedButton);
+    
+    const deleteButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-trash"></i>';
+    completedButton.classList.add('delete_btn');
+    todoDiv.appendChild(deleteButton);
+    
+    todoList.appendChild(todoDiv);
+}
